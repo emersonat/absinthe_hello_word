a0f9793a40442411e620e2a0361101649554797b
emersonat <emersonat@ig.com.br>
2018-11-10 01:50:25 -0200

    primeiro commit

---
+++ lib/community_web/router.ex
@@ -0,0 +1,22 @@
+defmodule CommunityWeb.Router do
+  use CommunityWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", CommunityWeb do
+    pipe_through :api
+  end
+
+  scope "/" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+            schema: CommunityWeb.Schema,
+            interface: :simple,
+            context: %{pubsub: CommunityWeb.Endpoint}
+  end
+
+
+end
