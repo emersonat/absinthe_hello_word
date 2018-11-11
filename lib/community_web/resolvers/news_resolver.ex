bc22d40e2d87466a1904eca0bf66b1453d6c8f50
emersonat <emersonat@ig.com.br>
2018-11-11 15:28:24 -0200

    ajustes

--- lib/community_web/resolvers/news_resolver.ex
+++ lib/community_web/resolvers/news_resolver.ex
@@ -5,7 +5,22 @@ defmodule CommunityWeb.NewsResolver do
 
   def all_links(_root, _args, _info) do
     links = News.list_links()
+    IO.puts "*******************************************************"
+    IO.inspect(links)
     {:ok, links}
   end
+
+
+  def create_link(_root, args, _info) do
+    # TODO: add detailed error message handling later
+    case News.create_link(args) do
+      {:ok, link} ->
+        {:ok, link}
+      _error ->
+        {:error, "could not create link"}
+    end
+  end
+
+
 
 end
--- lib/community_web/schema.ex
+++ lib/community_web/schema.ex
@@ -3,12 +3,13 @@ defmodule CommunityWeb.Schema do
 
   alias CommunityWeb.NewsResolver
 
-  object :link do
-    field :id, non_null(:id)
-    field :url, non_null(:string)
-    field :description, non_null(:string)
-  end
+#  object :link do
+#    field :id, non_null(:id)
+#    field :url, non_null(:string)
+#    field :description, non_null(:string)
+#  end
 
+  import_types(Community.News.Link)
 
 
   query do
@@ -18,10 +19,17 @@ defmodule CommunityWeb.Schema do
       resolve &NewsResolver.all_links/3
     end
 
+   end
 
+   mutation do
+     field :create_link, :link do
 
+       arg :url, non_null(:string)
+       arg :description, non_null(:string)
 
+       resolve &NewsResolver.create_link/3
 
+     end
   end
 
 
